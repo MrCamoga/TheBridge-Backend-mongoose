81fdc509dd5f94ab36b4294e164c532dbb2b6836
MrCamoga <mrcamoga@gmail.com>
2025-06-15 19:48:53 +0200

    Routes

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.use(express.json());
 
 app.get('/', (req,res) => res.send('Welcome!'));
 
+const routes = ['users','auth','posts','comments'];
+
+routes.forEach(route => app.use('/'+route, require('./routes/'+route)));
+
 app.listen(PORT, () => console.log(`Server listening on 0.0.0.0:${PORT}`));
