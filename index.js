8bf76dc175173b723d183fa1bcb7774459294f2c
MrCamoga <mrcamoga@gmail.com>
2025-06-18 19:46:06 +0200

    Fixed dbConnection not called

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const { PORT } = require('./config/');
 
 const app = express();
 
-app.use(express.json());
+const dbConnection = require('./config/db');
+dbConnection();
 
+app.use(express.json());
 app.get('/', (req,res) => res.send('Welcome!'));
 
 const routes = ['users','auth','posts','comments'];
-
 routes.forEach(route => app.use('/'+route, require('./routes/'+route)));
 
 app.listen(PORT, () => console.log(`Server listening on 0.0.0.0:${PORT}`));
